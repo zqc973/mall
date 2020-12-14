311ac13f5b39c9eccaac62092f59ec8a9d21ee8b
zqc973 <1101570660@qq.com>
2020-12-14 17:35:21 +0800

    111

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,35 +6,172 @@
           <div>购物街</div>
         </template>
       </nav-bar>
+      <recommend-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control class="tabcontrol" :titles="['流行', '新款', '精选']"/>
+
+    <ul>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import RecommendView from './childComps/RecomendView.vue'
+import FeatureView from './childComps/FeatureView.vue'
+
 import NavBar from 'components/common/navbar/NavBar.vue'
+import TabControl from 'components/content/tabControl/TabControl.vue'
+
 import {getHomeMultidata} from 'network/home.js'
+
 
 export default {
   name: "Home",
-  components: {
-    NavBar
+  components: {    
+    RecommendView,
+    FeatureView,
+
+    NavBar,
+    TabControl
   },
   data() {
     return {
-      result : null
+      banners : [],
+      recommends: [],
+      goods: {
+        pop: {page: 0, list: []},
+        new: {page: 0, list: []},
+        sell: {page: 0, list: []}
+      }
     }    
   },
   created() {
     getHomeMultidata().then(res => {
-      console.log(res);
-      this.result = res;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
     })
   }
 }
 
 </script>
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav{
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tabcontrol{
+    position: sticky;
+    top: 43px;
   }
 </style>
